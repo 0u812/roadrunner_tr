6f2bbff857a250035164c5f48cdc1f950f94b0a5
0u812 <0u812@users.noreply.github.com>
2018-09-17 08:04:30 -0700

    add script dir to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 
 from setuptools import setup
+from os.path import join
 
 setup(name='libroadrunner_tr',
       version='0.1.0',
       description='SBML Test Suite Runner for libRoadRunner',
       author='J. Kyle Medley',
       packages=['roadrunner_tr'],
+      scripts=[join('bin','roadrunner_tr'),
       install_requires=[
         'libroadrunner',
         'pandas',
